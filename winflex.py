350d779063ea91636687304ef5a481f3d78184bc
Hatz <tim.cui.yh@gmail.com>
2018-01-04 15:55:57 -0500

    Completed illustration flow for AIG GUL. Created main() so script works on run. made printouts more verbose

--- winflex.py
+++ winflex.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 
 class Flexor(object):
@@ -32,8 +33,17 @@ class Flexor(object):
         # Open illustrations tool
         illustration_front = self.browser.find_element_by_css_selector("a.shortcut:nth-child(1)")
         illustration_front.click()
+        try:
+            wait = WebDriverWait(self.browser, 5)
+            element = wait.until(
+                EC.text_to_be_present_in_element(
+                    (By.CSS_SELECTOR, "#divWinFlexShortcuts > div.panel-body > div > a:nth-child(1) > span"),
+                    "Start a New Case"))
+            print("Initialization Complete")
+        except TimeoutException:
+            print("Failed to Initialize")
 
-    def open_product(self, name, product):
+    def new_case(self):
         """Selects the product and get into the forms page"""
         new_case = self.browser.find_element_by_css_selector("a.shortcut:nth-child(1)")
         new_case.click()
@@ -51,13 +61,15 @@ class Flexor(object):
             "#Insured\.Name\|0 > div:nth-child(2) > input:nth-child(1)")
         gender_select = Select(
             self.browser.find_element_by_css_selector("#Insured\.Sex\|0 > div:nth-child(2) > select:nth-child(1)"))
+        class_select = Select(
+            self.browser.find_element_by_css_selector(r"#Insured\2e Class\7c 0 > div.labelPosLeft > select"))
         age = self.browser.find_element_by_css_selector("#Insured\.Age\|0 > div:nth-child(2) > input:nth-child(1)")
 
         client_name.send_keys('Valued Client')
         gender_select.select_by_visible_text("Female")
+        class_select.select_by_visible_text("Preferred Non-Tobacco")
         age.clear()
-        client_age = '35'
-        age.send_keys(client_age)
+        age.send_keys("35")
 
     def fill_solve(self):
         """ Populates the solve for tab"""
@@ -76,33 +88,70 @@ class Flexor(object):
         years_to_pay_input = self.browser.find_element_by_css_selector(
             "#Textbox_Policy\.PremiumYears\.Years\|0 > div:nth-child(1) > input:nth-child(1)")
         years_to_pay_input.clear()
-        years_to_pay_input.send_keys("35")
+        years_to_pay_input.send_keys("5")
 
     def update_field(self, element, value):
         update_element = self.browser.find_element_by_css_selector(element)
         update_element.clear()
         update_element.send_keys(value)
+        update_element.send_keys(Keys.TAB)
 
     def calculate(self):
         calculate = self.browser.find_element_by_css_selector("#btnClientCalc")
         calculate.click()
+        print("Calculating")
         try:
-            wait = WebDriverWait(self.browser, 15)
+            wait = WebDriverWait(self.browser, 30)
             element = wait.until(
                 EC.text_to_be_present_in_element(
                     (By.CSS_SELECTOR, "#divSnapshotPanel > div:nth-child(1) > div:nth-child(1)"), "Snapshot"))
-            print("calculation complete")
+            print("Calculation Complete")
         except TimeoutException:
-            print("failed")
+            print("Failed to calculate")
 
-    def get_premiums(self):
-        annual_premium = self.browser.find_element_by_css_selector("td.columnTextQC:nth-child(6)")
-        target_premium = self.browser.find_element_by_css_selector("td.columnTextQC:nth-child(7)")
+    def print_results(self):
+        annual_premium = self.browser.find_element_by_css_selector("#cbSendRow_103 > td:nth-child(6)")
+        target_premium = self.browser.find_element_by_css_selector("#cbSendRow_103 > td:nth-child(7)")
+        payment_duration = self.browser.find_element_by_css_selector("#cbSendRow_103 > td:nth-child(9)")
+        sacs = self.browser.find_element_by_css_selector("#cbSendRow_103 > td:nth-child(4)")
+        print("Insured is: \n" + sacs.text)
         print("Annual Premium: " + annual_premium.text + " " + "Target Premium: " + target_premium.text)
+        print("Payment duration is: " + payment_duration.text)
+        return int(annual_premium.text.replace(',', ''))
 
     def reset_form(self):
         recalculate = self.browser.find_element_by_css_selector("a.text-primary")
         recalculate.click()
+        print("Resetting..........")
+        try:
+            wait = WebDriverWait(self.browser, 5)
+            element = wait.until(
+                EC.text_to_be_present_in_element(
+                    (By.CSS_SELECTOR, "#QuestionTab1 > a"), "Solve For"))
+            print("Reset Complete")
+        except TimeoutException:
+            print("Failed to reset")
+
+
+def illustrate_suite(flexor, intervals):
+    """
+
+    :param flexor: uses the initialized winflex illustration object, Flexor
+    :param intervals: the different payment durations
+    :return: the target premium and annual premiums
+    """
+    payment_duration_field = "#Textbox_Policy\.PremiumYears\.Years\|0 > div:nth-child(1) > input:nth-child(1)"
+    solve_tab = flexor.browser.find_element_by_css_selector('#QuestionTab1 > a:nth-child(1)')
+    solve_tab.click()
+    premiums = []
+
+    for duration in intervals:
+        flexor.update_field(payment_duration_field, duration)
+        flexor.calculate()
+        premiums.append(flexor.print_results())
+        flexor.reset_form()
+
+    print(*premiums, sep="\n")
 
 
 def main():
@@ -110,3 +159,20 @@ def main():
     age_range = ["35","40","45","50","55","60","65"]
     name = 'AG - American General'
     product = 'Secure Lifetime GUL 3'
+
+    flex = Flexor()
+    flex.initialize()
+    flex.new_case()
+    flex.fill_client()
+    flex.fill_solve()
+
+    for age in age_range:
+        insured_tab = flex.browser.find_element_by_css_selector('#QuestionTab0 > a:nth-child(1)')
+        insured_tab.click()
+        flex.update_field("#Insured\.Age\|0 > div:nth-child(2) > input:nth-child(1)", age)
+        illustrate_suite(flex, intervals)
+        print("Age: " + age + " Completed")
+
+
+if __name__ == '__main__':
+    main()
