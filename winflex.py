a705cbaccbe33dc6ca465db231fa468d468457da
Hatzun <tim.cui.yh@gmail.com>
2018-10-01 17:14:45 -0400

    Shifted element locations to top of file and accessible as attributes of flexor object so they can be changed on the fly. Other misc changes to formatting and using updated CSS selectors.

--- winflex.py
+++ winflex.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementNotVisibleException
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,6 +11,19 @@ from selenium.webdriver.common.keys import Keys
 class Flexor(object):
     def __init__(self):
         self.browser = webdriver.Chrome()
+
+        """ field locations """
+        self.field_tp = "td.columnTextQC:nth-child(7)"
+        self.field_ap = "td.columnTextQC:nth-child(6)"
+        self.field_insured = "td.columnTextQC-center:nth-child(4)"
+        self.field_pd = "td.columnTextQC-center:nth-child(9)"
+        self.field_product = "#lbProductSelectorProducts > tbody > tr:nth-child(4) > td:nth-child(2)"
+        self.field_company = "#lbProductSelectorCompanies > tbody > tr:nth-child(6) > td"
+        self.field_duration = r"#Year_Policy\2e PremiumYears\7c 1 > div > div > input"
+
+        """ values """
+        self.health_class = "Preferred"
+        self.premium_option = "NLG Lifetime"
 
     def initialize(self):
         """
@@ -47,9 +61,10 @@ class Flexor(object):
         """Selects the product and get into the forms page"""
         new_case = self.browser.find_element_by_css_selector("a.shortcut:nth-child(1)")
         new_case.click()
-        company = self.browser.find_element_by_css_selector(
-            "#lbProductSelectorCompanies > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)")
+        company = self.browser.find_element_by_css_selector(self.field_company)
         company.click()
+        product = self.browser.find_element_by_css_selector(self.field_product)
+        product.click()
         create_client = self.browser.find_element_by_css_selector("#btnProductSelectorCreate")
         create_client.click()
 
@@ -66,8 +81,8 @@ class Flexor(object):
         age = self.browser.find_element_by_css_selector("#Insured\.Age\|0 > div:nth-child(2) > input:nth-child(1)")
 
         client_name.send_keys("Valued Client")
-        gender_select.select_by_visible_text("Male")
-        class_select.select_by_visible_text("Preferred Non-Tobacco")
+        gender_select.select_by_visible_text("Female")
+        class_select.select_by_visible_text(self.health_class)
         age.clear()
         age.send_keys("25")
 
@@ -79,14 +94,13 @@ class Flexor(object):
             self.browser.find_element_by_css_selector("#Policy\.SolveFor\|0 > div:nth-child(2) > select:nth-child(1)"))
         solve_for.select_by_visible_text('Premium')
         solve_option = Select(
-            self.browser.find_element_by_css_selector("#Policy\.Premium\|2 > div:nth-child(2) > select:nth-child(1)"))
-        solve_option.select_by_visible_text("Guarantee Premium")
+            self.browser.find_element_by_css_selector(r"#Policy\2e Premium\7c 2 > div.labelPosLeft > select"))
+        solve_option.select_by_visible_text(self.premium_option)
         payment_terms = Select(
             self.browser.find_element_by_css_selector(
-                "#Combo_Policy\.PremiumYears\|0 > div:nth-child(2) > select:nth-child(1)"))
+                r"#Combo_Policy\2e PremiumYears\7c 1 > div.labelPosLeft > select"))
         payment_terms.select_by_visible_text('Year')
-        years_to_pay_input = self.browser.find_element_by_css_selector(
-            "#Textbox_Policy\.PremiumYears\.Years\|0 > div:nth-child(1) > input:nth-child(1)")
+        years_to_pay_input = self.browser.find_element_by_css_selector(self.field_duration)
         years_to_pay_input.clear()
         years_to_pay_input.send_keys("5")
 
@@ -107,29 +121,36 @@ class Flexor(object):
                     (By.CSS_SELECTOR, "#divResults > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1)")
                     , "Illustration Results"))
             print("Calculation Complete")
+            try:
+                self.browser.find_element_by_css_selector('#closeHtmlModalButton').click()
+            except ElementNotVisibleException:
+                print("No close button")
         except TimeoutException:
             print("Failed to calculate")
 
     def print_results(self):
-        annual_premium = self.browser.find_element_by_css_selector("td.columnTextQC:nth-child(6)")
-        target_premium = self.browser.find_element_by_css_selector("td.columnTextQC:nth-child(7")
-        payment_duration = self.browser.find_element_by_css_selector("td.columnTextQC-center:nth-child(9)")
-        sacs = self.browser.find_element_by_css_selector("td.columnTextQC-center:nth-child(4)")
-        print("Insured is: \n" + sacs.text)
+        annual_premium = self.browser.find_element_by_css_selector(self.field_tp)
+        target_premium = self.browser.find_element_by_css_selector(self.field_ap)
+        payment_duration = self.browser.find_element_by_css_selector(self.field_pd)
+        insured_info = self.browser.find_element_by_css_selector(self.field_insured)
+        print("Insured is: \n" + insured_info.text)
         print("Annual Premium: " + annual_premium.text + " " + "Target Premium: " + target_premium.text)
         print("Payment duration is: " + payment_duration.text)
         return int(annual_premium.text.replace(',', ''))
 
     def get_tp(self):
-        target_premium = self.browser.find_element_by_css_selector("td.columnTextQC:nth-child(7)")
-        return int(target_premium.text.replace(',', ''))
+        target_premium = self.browser.find_element_by_css_selector(self.field_tp)
+        if target_premium.text != "N/A":
+            return int(target_premium.text.replace(',', ''))
+        else:
+            return 0
 
     def get_ap(self):
-        annual_premium = self.browser.find_element_by_css_selector("td.columnTextQC:nth-child(6)")
+        annual_premium = self.browser.find_element_by_css_selector(self.field_ap)
         return int(annual_premium.text.replace(',', ''))
 
     def get_insured(self):
-        insured_info = self.browser.find_element_by_css_selector("td.columnTextQC-center:nth-child(4)")
+        insured_info = self.browser.find_element_by_css_selector(self.field_insured)
         return insured_info.text
 
     def reset_form(self):
@@ -152,7 +173,7 @@ def illustrate_suite(flexor, intervals):
     :param intervals: the different payment durations
     :return: the target premium and annual premiums
     """
-    payment_duration_field = "#Textbox_Policy\.PremiumYears\.Years\|0 > div:nth-child(1) > input:nth-child(1)"
+    payment_duration_field = r"#Year_Policy\2e PremiumYears\7c 1 > div > div > input"
     solve_tab = flexor.browser.find_element_by_css_selector('#QuestionTab1 > a:nth-child(1)')
     solve_tab.click()
     premiums = []
@@ -169,6 +190,7 @@ def illustrate_suite(flexor, intervals):
         flexor.reset_form()
     print(insured)
     print(*premiums, sep="\n")
+
 
 def illustrate_other(flexor, intervals):
     """
@@ -202,18 +224,19 @@ def illustrate_other(flexor, intervals):
     print(insured)
     print(*premiums, sep="\n")
 
+
 def fills(flex):
     flex.new_case()
     flex.fill_client()
     flex.fill_solve()
 
 
-def illustrate_range(flex, range, interval):
-    for age in range:
+def illustrate_range(flex, ranges, intervals):
+    for age in ranges:
         insured_tab = flex.browser.find_element_by_css_selector('#QuestionTab0 > a:nth-child(1)')
         insured_tab.click()
         flex.update_field("#Insured\.Age\|0 > div:nth-child(2) > input:nth-child(1)", age)
-        illustrate_suite(flex, interval)
+        illustrate_suite(flex, intervals)
         print("Age: " + age + " Completed")
 
 
@@ -224,5 +247,5 @@ if __name__ == '__main__':
     illustrate_suite just runs the illustrations for that age at the intervals defined.
     """
 
-    interval = ["5", "10"]
-    age_range = ["25","30","35","40","45","50","55","60","65"]
+    interval = ["5", "10", "20"]
+    age_range = ["35", "40", "45", "50", "55", "60", "65"]
